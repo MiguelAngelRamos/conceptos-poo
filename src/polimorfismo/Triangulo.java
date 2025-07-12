47c8166a89f01ad5cb4481f2aba8bb875e992fa0
Miguel Ramos <return.infox@gmail.com>
2025-07-12 11:22:02 -0400

    codigo base

--- src/polimorfismo/Triangulo.java
+++ src/polimorfismo/Triangulo.java
@@ -1,5 +1,27 @@
 package polimorfismo;
 
-public class Triangulo {
+public class Triangulo extends Figura {
+
+  private double base;
+  private double altura;
+  private double lado1;
+  private double lado2;
+
+  public Triangulo(double base, double altura, double lado1, double lado2) {
+    this.base = base;
+    this.altura = altura;
+    this.lado1 = lado1;
+    this.lado2 = lado2;
+  }
+
+  @Override
+  public double calcularArea() {
+    return (base * altura) / 2;
+  }
+
+  @Override
+  public double calcularPerimetro() {
+    return base + lado1 + lado2;
+  }
   
 }
