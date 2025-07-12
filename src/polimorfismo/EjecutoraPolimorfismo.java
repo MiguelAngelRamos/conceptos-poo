915a97486aad277fb921bac30cc5bd092798b3c5
Miguel Ramos <return.infox@gmail.com>
2025-07-12 11:34:12 -0400

    poliformismo con herencia

--- src/polimorfismo/EjecutoraPolimorfismo.java
+++ src/polimorfismo/EjecutoraPolimorfismo.java
@@ -1,5 +1,20 @@
 package polimorfismo;
 
 public class EjecutoraPolimorfismo {
+
+  public static void main(String[] args) {
+    Figura [] figurasArray = new Figura[3];
+    figurasArray[0] = new Circulo(5);
+    figurasArray[1] = new Rectangulo(4, 6);
+    figurasArray[2] = new Triangulo(3, 4, 5, 6);
+
+    for (Figura figura : figurasArray) {
+      System.out.println("Área: " + figura.calcularArea());
+      System.out.println("Perímetro: " + figura.calcularPerimetro());
+      System.out.println();
+    }
+  }
+
   
+ 
 }
--- src/polimorfismo/Triangulo.java
+++ src/polimorfismo/Triangulo.java
@@ -7,11 +7,13 @@ public class Triangulo extends Figura {
   private double lado1;
   private double lado2;
 
+
   public Triangulo(double base, double altura, double lado1, double lado2) {
     this.base = base;
     this.altura = altura;
     this.lado1 = lado1;
     this.lado2 = lado2;
+   
   }
 
   @Override
